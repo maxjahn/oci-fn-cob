db75e752592a994a1897cc2dcaa2e2a255ba0e4a
maxjahn <max.jahn@gmail.com>
2020-11-01 23:06:06 +0100

    complete sample

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,12 @@
-FROM olegkunitsyn/gnucobol:3.1-dev
+FROM alpine:latest
 
+WORKDIR /opt
+
+RUN apk add tar libaio libnsl libc6-compat autoconf make g++ gmp-dev db-dev
+RUN wget https://sourceforge.net/projects/gnucobol/files/gnucobol/3.1/gnucobol-3.1-rc1.tar.gz/download -O gnucobol-3.1-rc1.tar.gz && \
+    tar xvfz gnucobol-3.1-rc1.tar.gz && cd gnucobol-3.1-rc1 && ./configure && make && make install && cd ..
+
+# install fn hotwrap 
 COPY --from=fnproject/hotwrap:latest  /hotwrap /hotwrap 
 COPY ./src .
 RUN cobc -x func.cob
--- src/func.cob
+++ src/func.cob
@@ -1,6 +1,25 @@
-      * Sample
+*******> Sample for running COBOL code with fn
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. hello.
+       PROGRAM-ID. FN_COB.
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           SELECT SYSIN ASSIGN TO KEYBOARD ORGANIZATION LINE SEQUENTIAL.
+       DATA DIVISION.
+       FILE SECTION.
+           FD SYSIN.
+           01 LN PIC X(80).
+               88 EOF VALUE HIGH-VALUES.
+       WORKING-STORAGE SECTION.
+       01 WS-STDIN GLOBAL PIC X(255).
+       01 W-IDX PIC 9(2) VALUE ZERO.     
        PROCEDURE DIVISION.
-       DISPLAY "Hello, world!".
+           ACCEPT WS-STDIN.
+           UNSTRING WS-STDIN DELIMITED ALL SPACE
+           INTO WS-STDIN COUNT W-IDX
+           IF W-IDX > 0 THEN
+               DISPLAY "Hello, " WS-STDIN(1:W-IDX) "!"
+             ELSE
+               DISPLAY "Hello, world!"
+           END-IF
        STOP RUN.
